c07fc44d4baa00cddd24588e35bca9645b4d8674
VanyaTopchik <gvanya97@mail.ru>
2021-06-03 12:55:29 +0300

    Added fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import com.google.common.math.Quantiles;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
@@ -10,6 +12,7 @@ public class Main {
         JSONTicketsParser jsonTicketsParser = new JSONTicketsParser();
         Tickets tickets = jsonTicketsParser.parse(fileName);//Парсим и получаем билеты
         long count = 0, sum = 0;
+        ArrayList<Long> times = new ArrayList<>();
         for (Ticket ticket : tickets.getTickets()) {//вычисление времени полета для каждого полета
             //Пример: "departure_date": "12.05.18", "departure_time": "16:20",
             String time1 = ticket.getDeparture_date() + " " + ticket.getDeparture_time();
@@ -18,6 +21,7 @@ public class Main {
             Date departureDate = dateFormat.parse(time1);
             Date arrivalDate = dateFormat.parse(time2);
             long time = arrivalDate.getTime() - departureDate.getTime();
+            times.add(time);
             sum += time;
             count++;
         }
@@ -25,11 +29,11 @@ public class Main {
         long second = (avgTime / 1000) % 60;
         long minute = (avgTime / (1000 * 60)) % 60;
         long hour = (avgTime / (1000 * 60 * 60)) % 24;
-        System.out.printf("Среднее время полета между городами: %02d:%02d:%02d.\n", hour, minute, second);
-        long avgTime90 = (long) (avgTime * 0.9);
-        long second90 = (avgTime90 / 1000) % 60;
-        long minute90 = (avgTime90 / (1000 * 60)) % 60;
-        long hour90 = (avgTime90 / (1000 * 60 * 60)) % 24;
-        System.out.printf("90-й процентиль среднего времени полета между городами Владивосток и Тель-Авив: %02d:%02d:%02d.\n", hour90, minute90, second90);
+        System.out.printf("Среднее время полета между городами Владивосток и Тель-Авив: %02d:%02d:%02d.\n", hour, minute, second);
+        double percentile90 = Quantiles.percentiles().index(90).compute(times);
+        long second90 = (long) ((percentile90 / 1000) % 60);
+        long minute90 = (long) ((percentile90 / (1000 * 60)) % 60);
+        long hour90 = (long) ((percentile90 / (1000 * 60 * 60)) % 24);
+        System.out.printf("90-й процентиль времени полета между городами Владивосток и Тель-Авив: %02d:%02d:%02d.\n", hour90, minute90, second90);
     }
 }
